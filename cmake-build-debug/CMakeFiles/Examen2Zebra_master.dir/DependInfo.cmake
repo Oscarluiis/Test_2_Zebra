8832b511e9d1b98d35383da0d4980f44942de8b9
Oscarluiis <tinocoscar11@unitec.edu>
2017-09-18 08:36:06 -0600

    Resuelto...

---
+++ cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/LuiizMusic11/Desktop/Hola/Examen2Zebra-master/Evaluador.cpp" "/Users/LuiizMusic11/Desktop/Hola/Examen2Zebra-master/cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/Evaluador.cpp.o"
+  "/Users/LuiizMusic11/Desktop/Hola/Examen2Zebra-master/Libro.cpp" "/Users/LuiizMusic11/Desktop/Hola/Examen2Zebra-master/cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/Libro.cpp.o"
+  "/Users/LuiizMusic11/Desktop/Hola/Examen2Zebra-master/NodoBinario.cpp" "/Users/LuiizMusic11/Desktop/Hola/Examen2Zebra-master/cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/NodoBinario.cpp.o"
+  "/Users/LuiizMusic11/Desktop/Hola/Examen2Zebra-master/main.cpp" "/Users/LuiizMusic11/Desktop/Hola/Examen2Zebra-master/cmake-build-debug/CMakeFiles/Examen2Zebra_master.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
